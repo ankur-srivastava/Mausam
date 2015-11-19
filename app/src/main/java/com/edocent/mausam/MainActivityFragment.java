0059a2a4cf94274b151de6c62c976b1c4c57182b
Ankur Srivastava <ankurlkw@gmail.com>
2015-11-19 09:43:31 +0530

    Added Adapter

--- app/src/main/java/com/edocent/mausam/MainActivityFragment.java
+++ app/src/main/java/com/edocent/mausam/MainActivityFragment.java
@@ -5,11 +5,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    ListView forecastListView;
 
     public MainActivityFragment() {
     }
@@ -17,6 +23,19 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        forecastListView = (ListView) view.findViewById(R.id.listview_forecast);
+
+        ArrayList<String> forecaseArray = new ArrayList<>();
+        forecaseArray.add("Today - Sunny - 88/63");
+        forecaseArray.add("Tomorrow - Foggy - 88/63");
+
+        ArrayAdapter<String> forecastAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview, forecaseArray);
+        forecastListView.setAdapter(forecastAdapter);
+
+        return view;
     }
 }
