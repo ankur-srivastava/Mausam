be3d7283c2ee90e71fc26ca32b4a6d44248fb8f3
Ankur Srivastava <ankurlkw@gmail.com>
2015-11-30 12:24:56 +0530

    Added Binder for Temperature Pref

--- app/src/main/java/com/edocent/mausam/SettingsActivity.java
+++ app/src/main/java/com/edocent/mausam/SettingsActivity.java
@@ -19,6 +19,7 @@ public class SettingsActivity extends PreferenceActivity
         // updated when the preference changes.
         // TODO: Add preferences
         bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_temp_key)));
     }
  
     /**
@@ -43,8 +44,6 @@ public class SettingsActivity extends PreferenceActivity
         String stringValue = value.toString();
  
         if (preference instanceof ListPreference) {
-            // For list preferences, look up the correct display value in
-            // the preference's 'entries' list (since they have separate labels/values).
             ListPreference listPreference = (ListPreference) preference;
             int prefIndex = listPreference.findIndexOfValue(stringValue);
             if (prefIndex >= 0) {
