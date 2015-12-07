db9bbda5c18f18e5ef1b881e41cd429807384fdc
Ankur Srivastava <ankurlkw@gmail.com>
2015-12-07 09:30:43 +0530

    Shared Pref Intent

--- app/src/main/java/com/edocent/mausam/DetailActivityFragment.java
+++ app/src/main/java/com/edocent/mausam/DetailActivityFragment.java
@@ -3,7 +3,12 @@ package com.edocent.mausam;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -14,8 +19,11 @@ import android.widget.TextView;
 public class DetailActivityFragment extends Fragment {
 
     TextView forecastTextView;
+    public static final String FORECAST_HASHTAG = "#Mausam";
+    String forecastString;
 
     public DetailActivityFragment() {
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -28,11 +36,48 @@ public class DetailActivityFragment extends Fragment {
         if(getActivity().getIntent() != null){
             Intent intent = getActivity().getIntent();
             if(intent.getExtras() != null){
-                String forecastText = intent.getExtras().getString(MainActivity.INTENT_EXTRA);
-                forecastTextView.setText(forecastText);
+                forecastString = intent.getExtras().getString(MainActivity.INTENT_EXTRA);
+                forecastTextView.setText(forecastString);
             }
         }
 
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_detail, menu);
+        super.onCreateOptionsMenu(menu, menuInflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(getActivity(), SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.shareId) {
+            ShareActionProvider shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+            if(shareActionProvider != null){
+                shareActionProvider.setShareIntent(createSharedIntent());
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private Intent createSharedIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, forecastString + FORECAST_HASHTAG);
+        return shareIntent;
     }
 }
